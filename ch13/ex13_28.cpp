c53d7197a2b6f326d36a20b8383fe8a895b994c2
sulvto <sulvto@gmail.com>
2018-04-17 00:00:14 +0800

    Update ch13

---
+++ ch13/ex13_28.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace::std;
+
+class TreeNode {
+public:
+
+	TreeNode(): count(0), left(nullptr), right(nullptr) {}
+
+	TreeNode(const string &v): value(v), count(0), left(nullptr), right(nullptr)	{}
+
+	TreeNode(const TreeNode &tn): value(tn.value), count(tn.count), 
+		left(tn.left), right(tn.right)  {}
+
+	~TreeNode();
+
+private:
+	string value;
+	int count;
+	TreeNode *left;
+	TreeNode *right;
+};
+
+TreeNode::~TreeNode() 
+{
+	delete left;
+	delete right;
+}
+		
+
+class BinStrTree {
+public:
+	BinStrTree(): root(nullptr) {}
+
+	BinStrTree(TreeNode *tn): root(tn) {}
+
+	BinStrTree(const BinStrTree &bst): root(bst.root) {}
+
+	~BinStrTree() = default;
+private:
+	TreeNode *root;
+};
+
+int main () 
+{
+	BinStrTree bst;
+}
